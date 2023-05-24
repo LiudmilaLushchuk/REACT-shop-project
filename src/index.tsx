89edb20c7f81f10dffd535e253f020ca0c1cad72
Liudmyla Lushchuk <lushchuk.l@gmail.com>
2023-05-24 19:41:10 +0200

    React Component

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,42 @@ import ReactDOM from 'react-dom/client'
 // const title = React.createElement('h1', { id: 'title' }, 'Hello React')
 
 // JSX
-const app = (
-    <div>
-        <h1 id="text">Hello App</h1>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam nulla
-            possimus commodi ea animi, numquam culpa consectetur cumque sapiente
-            eaque eos beatae temporibus delectus quas eius ipsam explicabo
-            adipisci ducimus!
-        </p>
-    </div>
-)
+// const app = (
+//     <div>
+//         <h1 id="text">Hello App</h1>
+//         <p>
+//             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam nulla
+//             possimus commodi ea animi, numquam culpa consectetur cumque sapiente
+//             eaque eos beatae temporibus delectus quas eius ipsam explicabo
+//             adipisci ducimus!
+//         </p>
+//     </div>
+// )
+
+// Component
+const App = () => {
+    return (
+        <div>
+            <h1>Hello App component</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+                placeat, illum ut ab perferendis saepe consectetur aliquid nemo
+                vero officia a optio ratione ex corrupti, enim asperiores,
+                dolores accusantium pariatur?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+                voluptatem consectetur esse accusamus facere laboriosam animi
+                aperiam cupiditate facilis commodi ex sapiente ullam, architecto
+                in libero optio, ab alias. Praesentium?
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
