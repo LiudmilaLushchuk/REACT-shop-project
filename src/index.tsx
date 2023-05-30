f78696dbffe9393e4617968579847443b0db3771
Liudmyla Lushchuk <lushchuk.l@gmail.com>
2023-05-30 11:23:42 +0200

    added absolute imports, App and Header components

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style.css'
-import Title from './Title'
-import { Content } from './Content'
-
-const App = () => {
-    return (
-        <>
-            <Title title="App" />
-            <Title title="React" num={1} />
-            <Content
-                text1="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                A laudantium tempore, voluptas blanditiis possimus pariatur 
-                atque reprehenderit nemo debitis dignissimos voluptatem 
-                quisquam itaque nostrum? Pariatur commodi libero fugit 
-                praesentium dignissimos!"
-                text2="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                A laudantium tempore, voluptas blanditiis possimus pariatur 
-                atque reprehenderit nemo debitis dignissimos voluptatem 
-                quisquam itaque nostrum? Pariatur commodi libero fugit 
-                praesentium dignissimos!"
-            />
-        </>
-    )
-}
+import App from 'Container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
