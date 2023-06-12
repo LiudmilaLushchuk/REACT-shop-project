e8e83bfdd091431cc8b67cd1b5b03946ff5628da
Liudmyla Lushchuk <lushchuk.l@gmail.com>
2023-06-12 17:14:53 +0200

    added addProductToCart

--- src/Container/Main/Main.tsx
+++ src/Container/Main/Main.tsx
@@ -1,14 +1,16 @@
 import { Container } from '@mui/material'
 import Home from 'pages/Home/Home'
 
-type Props = {}
+type Props = {
+  addProductToCart: (count: number, price: number) => void
+}
 
-const Main = (props: Props) => {
-    return (
-        <Container sx={{ padding: '40px 0' }}>
-            <Home />
-        </Container>
-    )
+const Main = ({ addProductToCart }: Props) => {
+  return (
+    <Container sx={{ padding: '40px 0' }}>
+      <Home addProductToCart={addProductToCart} />
+    </Container>
+  )
 }
 
 export default Main
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
 import ProductList from 'components/Products/ProductList'
 
-type Props = {}
+type Props = {
+  addProductToCart: (count: number, price: number) => void
+}
 
-const Home = (props: Props) => {
-    return (
-        <>
-            <ProductList />
-        </>
-    )
+const Home = ({ addProductToCart }: Props) => {
+  return (
+    <>
+      <ProductList addProductToCart={addProductToCart} />
+    </>
+  )
 }
 
 export default Home
